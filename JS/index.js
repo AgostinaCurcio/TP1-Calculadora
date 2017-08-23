b680796597364c65206a925632a8ed2b8171a5e8
Agostina Curcio <agostinacurcio@hotmail.com>
2017-08-23 18:34:14 -0300

    TP1-Calculadora

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+
+var Numero= (prompt("Ingresa un número"))
+
+var Numero2= (prompt ("Ahora ingresa otro número para sumarle"))
+
+var Numero3= (prompt("Ahora vamos a restar, ingresa un número"))
+
+var Numero4= (prompt ("Ingresa un número para restarle"))
+
+var Numero5= (prompt("¿Y la multiplicación? Ingresa un número"))
+
+var Numero6= (prompt ("Ahora ingresa el número por el que quieras multiplicarlo"))
+
+var Numero7= (prompt("Para terminar dividamos. Ingresa un número"))
+
+var Numero8= (prompt ("Ahora el último número que será el divisor"))
+
+alert(Number(Numero) + Number(Numero2))
+alert(Number(Numero3) - Number(Numero4))
+alert(Number(Numero5) * Number(Numero6))
+alert(Number(Numero7) / Number(Numero8))
+
+
+
+
